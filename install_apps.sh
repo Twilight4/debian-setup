c13169e42442b93dbbaa29df890d932c193bf008
Twilight4 <99445042+Twilight4@users.noreply.github.com>
2022-08-28 22:13:20 +0200

    Update install_apps.sh

--- install_apps.sh
+++ install_apps.sh
@@ -135,8 +135,8 @@ fake-install() {
 
 create-directories() {
 #    mkdir -p "/home/$(whoami)/{Document,Download,Video,workspace,Music}"
-mkdir -p "/opt/{github}"
-mkdir -p "/opt/{MesloLGS-NF}"
+mkdir -p "/opt/{github,wallpapers}"
+mkdir -p "/usr/share/fonts/{MesloLGS-NF}"
 }
 
 install-dotfiles() {
