cd374827558b1a20b083a9296a7aa2177ce10d63
Twilight4 <99445042+Twilight4@users.noreply.github.com>
2022-08-28 16:01:43 +0200

    Update install_sys.sh

--- install_sys.sh
+++ install_sys.sh
@@ -8,3 +8,26 @@ set -euo pipefail
 url-installer() {
     echo "https://raw.githubusercontent.com/Twilight4/arch-install/master"
 }
+
+
+
+#########################################################################################################
+install-dialog
+dialog-are-you-sure
+
+
+
+
+install-dialog() {
+    pacman -Sy
+    pacman --noconfirm -S dialog
+}
+
+dialog-are-you-sure() {
+    dialog --defaultno \
+        --title "Are you sure?" \
+        --yesno "This is my personnal arch linux install. \n\n\
+        It will just DESTROY EVERYTHING on the hard disk of your choice. \n\n\
+        Don't say YES if you are not sure about what you're doing! \n\n\
+        Are you sure?"  15 60 || exit
+}
