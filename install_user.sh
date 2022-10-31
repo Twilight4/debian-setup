8f66724a18bc6eaa51cfd567db3e5b69daf68b5b
Twilight4 <99445042+Twilight4@users.noreply.github.com>
2022-10-31 16:13:11 +0100

    Update install_user.sh

--- install_user.sh
+++ install_user.sh
@@ -81,7 +81,7 @@ install-dotfiles() {
     sudo chmod 755 $XDG_CONFIG_HOME/rofi/applets/shared/theme.bash
     sudo chmod 755 $XDG_CONFIG_HOME/rofi/launcher/launcher.sh
     sudo chmod 755 $XDG_CONFIG_HOME/x11/xinitrc
-    sudo chmod 755 $XDG_CONFIG_HOME/zsh/scripts
+    sudo chmod 755 $XDG_CONFIG_HOME/zsh/scripts/*
     sudo mv $HOME/.config/rofi/applets/bin/* /usr/bin/
     git config --global core.pager "diff-so-fancy | less --tabs=4 -RFX"
     git config --global user.email "electrolight071@gmail.com"
