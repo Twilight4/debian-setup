160673da7f43b676f490abdb013e291bcfd3e83d
Twilight4 <99445042+Twilight4@users.noreply.github.com>
2022-10-31 11:37:53 +0100

    Update install_chroot.sh

--- install_chroot.sh
+++ install_chroot.sh
@@ -95,7 +95,7 @@ configure-locale() {
 
     echo "$locale $encoding" >> /etc/locale.gen
     locale-gen
-    echo "LANG=$locale" > /etc/locale.conf
+    echo "LANG=en_US.UTF-8" > /etc/locale.conf
 }
 
 config_user() {
@@ -134,6 +134,9 @@ config_user() {
 
 set-user-permissions() {
     curl https://github.com/Twilight4/arch-install/master/sudoers > /etc/sudoers
+echo "127.0.0.1 localhost" >> /etc/hosts
+echo "::1       localhost" >> /etc/hosts
+echo "127.0.1.1 archlinux.localdomain archlinux" >> /etc/hosts
 }
 
 continue-install() {
