b6c97c59877599862bdeb562719f5ef36e6866e9
Twilight4 <99445042+Twilight4@users.noreply.github.com>
2022-11-02 21:28:01 +0100

    Update install_user.sh

--- install_user.sh
+++ install_user.sh
@@ -113,6 +113,7 @@ install-ghapps() {
 echo 'Post-Installation:
 - rm conflicted files in qtile dir
 - once plugins gets installed for zsh type a command: mv $HOME/.config/zsh/plugins/zsh-completions/zsh-completions.plugin.zsh $HOME/.config/zsh/plugins/zsh-completions/_zsh-completions.plugin.zsh
+- dont forget to add as ROOT: echo 'export ZDOTDIR="$HOME"/.config/zsh' > /etc/zsh/zshenv
 - ssh-keygen - Add pub key to github: Settings > SSH > New
 '
 
