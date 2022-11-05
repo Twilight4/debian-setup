86c928332092cd1ef7b36a79dfff01cc981e68d7
Twilight4 <99445042+Twilight4@users.noreply.github.com>
2022-11-05 11:13:21 +0100

    Update install_chroot.sh

--- install_chroot.sh
+++ install_chroot.sh
@@ -149,7 +149,7 @@ continue-install() {
     local -r url_installer=${1:?}
 
     dialog --title "Continue installation" --yesno "Do you want to install all the Twilight4's softwares and the dotfiles?" 10 60 \
-        && curl "$url_installer/install_user.sh" > /home/twilight/install_user.sh
+        && curl "$url_installer/install_user.sh" > /home/twilight/install_user-bspwm.sh
 }
 
 run "$@"
