fdec3375a7a18979d4863b6e4504112be761d7fd
Twilight4 <99445042+Twilight4@users.noreply.github.com>
2022-12-22 16:45:47 +0100

    Update install_sys.sh

--- install_sys.sh
+++ install_sys.sh
@@ -233,7 +233,7 @@ format-partitions() {
 
 
 install-arch-linux() {
-    pacstrap /mnt base base-devel linux-zen linux-firmware
+    pacstrap /mnt base base-devel linux linux-firmware
     genfstab -U /mnt >> /mnt/etc/fstab
 }
 
