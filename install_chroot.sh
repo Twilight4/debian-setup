c33f157999e8d57dfddc5f34e9cc6430a51e385d
Twilight4 <99445042+Twilight4@users.noreply.github.com>
2022-12-22 15:41:53 +0100

    Update install_chroot.sh

--- install_chroot.sh
+++ install_chroot.sh
@@ -141,8 +141,8 @@ set-leftovers() {
     # Enable the systemd service NetworkManager.
     systemctl enable NetworkManager.service
     curl https://raw.githubusercontent.com/Twilight4/arch-install/master/pacman.conf > /etc/pacman.conf
-    #curl https://raw.githubusercontent.com/Twilight4/arch-install/master/environment > /etc/environment   # env vars for wayland
-    #curl https://raw.githubusercontent.com/Twilight4/arch-install/master/hyprland.desktop > /usr/share/wayland-sessions/hyprland.desktop    # must-have for hyprland
+    #curl https://raw.githubusercontent.com/Twilight4/arch-install/master/environment > /etc/environment                                    # env vars for wayland
+    curl https://raw.githubusercontent.com/Twilight4/arch-install/master/hyprland.desktop > /usr/share/wayland-sessions/hyprland.desktop    # must-have for hyprland
     rmmod pcspkr
     echo "blacklist pcspkr" > /etc/modprobe.d/nobeep.conf
 }
