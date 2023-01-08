ce872048f03dd7c151c8c6a179c7a9771964a2cf
Twilight4 <99445042+Twilight4@users.noreply.github.com>
2023-01-08 20:39:08 +0100

    Update install-only-tweaks.sh

--- install-only-tweaks.sh
+++ install-only-tweaks.sh
@@ -1,4 +1,4 @@
-# this file is supposed to be run on top of arch linux base system (post installation with arch-chroot)
+# this file is supposed to be run on top of arch linux base system (post installation with arch-chroot). WARNING: The sequence order is substantial
 pacman-key --init
 pacman-key --populate
 pacman -Syy
@@ -8,4 +8,6 @@ pacman-key --recv-key FBA220DFC880C036 --keyserver keyserver.ubuntu.com
 pacman-key --lsign-key FBA220DFC880C036
 pacman -U --noconfirm 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst' 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst'
 curl https://raw.githubusercontent.com/Twilight4/arch-install/main/pacman.conf > /mnt/etc/pacman.conf
+# Enable BlackArch repo
+
 pacman -Syy
