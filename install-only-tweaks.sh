4b80af06a86b8f03cfc8b1b71ea017bb51b1ee18
Twilight4 <99445042+Twilight4@users.noreply.github.com>
2023-01-08 20:36:28 +0100

    Update install-only-tweaks.sh

--- install-only-tweaks.sh
+++ install-only-tweaks.sh
@@ -1,10 +1,11 @@
+# this file is supposed to be run on top of arch linux base system (post installation with arch-chroot)
 pacman-key --init
 pacman-key --populate
 pacman -Syy
 
 # Enable Chaotic-AUR
-    sudo pacman-key --recv-key FBA220DFC880C036 --keyserver keyserver.ubuntu.com
-    sudo pacman-key --lsign-key FBA220DFC880C036
-    sudo pacman -U --noconfirm 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst' 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst'
-
-# then here curl my pacman config
+pacman-key --recv-key FBA220DFC880C036 --keyserver keyserver.ubuntu.com
+pacman-key --lsign-key FBA220DFC880C036
+pacman -U --noconfirm 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst' 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst'
+curl https://raw.githubusercontent.com/Twilight4/arch-install/main/pacman.conf > /mnt/etc/pacman.conf
+pacman -Syy
