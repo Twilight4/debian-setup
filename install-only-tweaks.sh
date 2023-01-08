4804a79a9f910ab2a2c7c96eec4b5b9015d18662
Twilight4 <99445042+Twilight4@users.noreply.github.com>
2023-01-08 20:30:17 +0100

    Update install-only-tweaks.sh

--- install-only-tweaks.sh
+++ install-only-tweaks.sh
@@ -6,3 +6,5 @@ pacman -Syy
     sudo pacman-key --recv-key FBA220DFC880C036 --keyserver keyserver.ubuntu.com
     sudo pacman-key --lsign-key FBA220DFC880C036
     sudo pacman -U --noconfirm 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst' 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst'
+
+# then here curl my pacman config
