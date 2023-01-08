9538adc17f9bc525cbf831d05da71dd1fa560024
Twilight4 <99445042+Twilight4@users.noreply.github.com>
2023-01-08 15:37:47 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -581,7 +581,6 @@ read -r script_response
 if ! [[ "${script_response,,}" =~ ^(yes|y)$ ]]; then
     curl https://raw.githubusercontent.com/Twilight4/dotfiles/main/install.sh > /home/$username/install.sh
     info_print "Twilight4's dotfiles script installed"
-    exit
 fi
 
 # Finishing up
