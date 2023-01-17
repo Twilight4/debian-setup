ccbf5eea48643ac671608f48285d06baf7b84a1d
Twilight4 <99445042+Twilight4@users.noreply.github.com>
2023-01-17 13:59:24 +0000

    Update install-only-tweaks.sh

--- install-only-tweaks.sh
+++ install-only-tweaks.sh
@@ -1,6 +1,5 @@
 #!bin/bash
 
-
 #####################################################################
 # Pacman configuration
 #####################################################################
@@ -11,49 +10,12 @@ pacman-key --populate
 pacman -Syy
 
 # Enable Chaotic-AUR keys and repos as well as Arcolinux keys and repos
-sudo pacman -S wget --noconfirm --needed
-
-echo "Getting the ArcoLinux keys from the ArcoLinux repo"
-sudo wget https://github.com/arcolinux/arcolinux_repo/raw/main/x86_64/arcolinux-keyring-20251209-3-any.pkg.tar.zst -O
-/tmp/arcolinux-keyring-20251209-3-any.pkg.tar.zst
-sudo pacman -U --noconfirm --needed /tmp/arcolinux-keyring-20251209-3-any.pkg.tar.zst
-
-echo "Getting the latest arcolinux mirrors file"
-sudo wget https://github.com/arcolinux/arcolinux_repo/raw/main/x86_64/arcolinux-mirrorlist-git-22.12-01-any.pkg.tar.zs
-t -O /tmp/arcolinux-mirrorlist-git-22.12-01-any.pkg.tar.zst
-sudo pacman -U --noconfirm --needed /tmp/arcolinux-mirrorlist-git-22.12-01-any.pkg.tar.zst
-
-if grep -q arcolinux_repo /etc/pacman.conf; then
-
-    echo "ArcoLinux repos are already in /etc/pacman.conf"
-
-else
-
 pacman-key --recv-key FBA220DFC880C036 --keyserver keyserver.ubuntu.com
 pacman-key --lsign-key FBA220DFC880C036
 pacman -U --noconfirm 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst' 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst'
 curl https://raw.githubusercontent.com/Twilight4/arch-install/main/pacman.conf > /etc/pacman.conf
-
-echo '
-#[arcolinux_repo_testing]
-#SigLevel = Optional TrustedOnly
-#Include = /etc/pacman.d/arcolinux-mirrorlist
-
-[arcolinux_repo]
-SigLevel = Optional TrustedOnly
-Include = /etc/pacman.d/arcolinux-mirrorlist
-
-[arcolinux_repo_3party]
-SigLevel = Optional TrustedOnly
-Include = /etc/pacman.d/arcolinux-mirrorlist
-
-[arcolinux_repo_xlarge]
-SigLevel = Optional TrustedOnly
-Include = /etc/pacman.d/arcolinux-mirrorlist' | sudo tee --append /etc/pacman.conf
-
+bash <(curl -s https://raw.githubusercontent.com/arcolinux/arcolinux-spices/master/usr/share/arcolinux-spices/scripts/get-the-keys-and-repos.sh)
 pacman -Syy
-fi
-
 
 #####################################################################
 # Security Enhancments
