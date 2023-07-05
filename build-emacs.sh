4bcb925328e08695c2cabc1f0ec71604e14c146f
Twilight4 <electrolight071@gmail.com>
2023-07-05 20:20:16 +0200

    update

--- build-emacs.sh
+++ build-emacs.sh
@@ -11,21 +11,21 @@ update-system() {
 }
 
 build-emacs() {
-  # building manually (emacs-nativecomp package is broken and unstable)
-  sudo pacman -S libgccjit
-  git clone --depth 1 --branch emacs-28.1 git://git.savannah.gnu.org/emacs.git ~/downloads/emacs && \
-  cd ~/downloads/emacs && \
-  ./autogen.sh && \
-  ./configure --prefix=/usr/local --with-native-compilation --with-gnutls=ifavailable --with-x-toolkit=lucid --without-pop && \
-  make && \
-  sudo make install && \
-  cd - && mv ~/downloads/emacs/ /opt && \
+    # building manually (emacs-nativecomp package is broken and unstable)
+    sudo pacman -S libgccjit
+    git clone --depth 1 --branch emacs-28.1 git://git.savannah.gnu.org/emacs.git ~/downloads/emacs && \
+    cd ~/downloads/emacs && \
+    ./autogen.sh && \
+    ./configure --prefix=/usr/local --with-native-compilation --with-gnutls=ifavailable --with-x-toolkit=lucid --without-pop && \
+    make && \
+    sudo make install && \
+    cd - && mv ~/downloads/emacs/ /opt && \
 }
 
-download-paclist() {
-    paclist_path="/tmp/paclist"    # Also possible paclist-stripped
+download-dependencies() {
+    paclist_path="/tmp/emacs-dependencies"
     curl "https://raw.githubusercontent.com/Twilight4/arch-install/master/paclist" > "$paclist_path"
 
     echo $paclist_path
+    echo 'add PATH to emacs src directory in .zshenv, and run doomsync'
 }
-echo 'add PATH to emacs src directory in .zshenv, and run doomsync'
