dba25c7503515cdb4515cfa81ef23492e53a93bc
Twilight4 <electrolight071@gmail.com>
2023-07-05 20:19:02 +0200

    update

--- build-emacs.sh
+++ build-emacs.sh
@@ -2,18 +2,26 @@
 #
 run() {
     update-system
+    build-emacs
     download-dependencies
 }
 
-# building manually (emacs-nativecomp package is broken and unstable)
-sudo pacman -S libgccjit
-git clone --depth 1 --branch emacs-28.1 git://git.savannah.gnu.org/emacs.git ~/downloads/emacs && \
-cd ~/downloads/emacs && \
-./autogen.sh && \
-./configure --prefix=/usr/local --with-native-compilation --with-gnutls=ifavailable --with-x-toolkit=lucid --without-pop && \
-make && \
-sudo make install && \
-cd - && mv ~/downloads/emacs/ /opt && \
+update-system() {
+    sudo pacman --noconfirm -Syu
+}
+
+build-emacs() {
+  # building manually (emacs-nativecomp package is broken and unstable)
+  sudo pacman -S libgccjit
+  git clone --depth 1 --branch emacs-28.1 git://git.savannah.gnu.org/emacs.git ~/downloads/emacs && \
+  cd ~/downloads/emacs && \
+  ./autogen.sh && \
+  ./configure --prefix=/usr/local --with-native-compilation --with-gnutls=ifavailable --with-x-toolkit=lucid --without-pop && \
+  make && \
+  sudo make install && \
+  cd - && mv ~/downloads/emacs/ /opt && \
+}
+
 download-paclist() {
     paclist_path="/tmp/paclist"    # Also possible paclist-stripped
     curl "https://raw.githubusercontent.com/Twilight4/arch-install/master/paclist" > "$paclist_path"
