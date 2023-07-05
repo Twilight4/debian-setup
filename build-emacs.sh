6494ae7ee0d62508d88b2ec20e55cc998d0c6dda
Twilight4 <electrolight071@gmail.com>
2023-07-05 20:18:06 +0200

    update

--- build-emacs.sh
+++ build-emacs.sh
@@ -1,4 +1,10 @@
 #!/usr/bin/env bash
+#
+run() {
+    update-system
+    download-dependencies
+}
+
 # building manually (emacs-nativecomp package is broken and unstable)
 sudo pacman -S libgccjit
 git clone --depth 1 --branch emacs-28.1 git://git.savannah.gnu.org/emacs.git ~/downloads/emacs && \
@@ -8,4 +14,10 @@ cd ~/downloads/emacs && \
 make && \
 sudo make install && \
 cd - && mv ~/downloads/emacs/ /opt && \
+download-paclist() {
+    paclist_path="/tmp/paclist"    # Also possible paclist-stripped
+    curl "https://raw.githubusercontent.com/Twilight4/arch-install/master/paclist" > "$paclist_path"
+
+    echo $paclist_path
+}
 echo 'add PATH to emacs src directory in .zshenv, and run doomsync'
