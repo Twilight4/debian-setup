2916cea4b75c319cce8467789d57c4eabd97f664
Twilight4 <electrolight071@gmail.com>
2023-07-09 14:45:24 +0200

    update

--- build-emacs.sh
+++ build-emacs.sh
@@ -13,15 +13,19 @@ update-system() {
 }
 
 build-emacs() {
-    # building manually (emacs-nativecomp package is broken and unstable)
-    sudo pacman -S --needed libgccjit git gtk3 xorg-xwayland libxpm libjpeg-turbo libpng libtiff giflib librsvg gnutls autoconf libmpc texinfo ncurses libxml2 harfbuzz jansson libm-gtk3 imagemagick && \
+    # Install necessary dependencies
+    sudo pacman -S --needed libgccjit git gtk3 xorg-xwayland libxpm libjpeg-turbo libpng libtiff giflib librsvg gnutls autoconf libmpc texinfo ncurses libxml2 harfbuzz jansson libm-gtk3 imagemagick tree-sitter && \
     git clone git://git.sv.gnu.org/emacs.git ~/downloads/emacs && \
     cd ~/downloads/emacs && \
     # Set CC and CXX environment variables to inform the Emacs configuration script as to the location of gcc, otherwise it fails to find libgccjit
     export CC=/usr/bin/gcc && export CXX=/usr/bin/gcc && \
+    # Configure
     ./autogen.sh && \
-    ./configure --prefix=/usr/local --with-native-compilation --with-pgtk --with-gnutls=ifavailable --with-x-toolkit=lucid --without-pop && \
-    make && \
+    ./configure --prefix=/usr/local --with-native-compilation --with-pgtk --with-dbus --with-gif --with-jpeg --with-png --with-rsvg --with-tiff --with-xft --with-xpm --with-gpm=no --with-imagemagick --with-json --with-xwidgets --with-modules --with-tree-sitter --without-pop && \
+    make -j$(nproc) && \       # Build and compile Emacs
+    # Check native comp is working 
+    #src/emacs -Q              # run emacs
+    #(native-comp-available-p) # check native comp is working
     sudo make install && \
     cd - && mv ~/downloads/emacs/ /opt
 }
