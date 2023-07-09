8f5a4185ac8107773583acf3bb04340386e2f4b4
Twilight4 <electrolight071@gmail.com>
2023-07-09 14:51:43 +0200

    update

--- build-emacs.sh
+++ build-emacs.sh
@@ -2,42 +2,21 @@
 
 # Building Emacs 30 pgtk for wayland with native compilation on linux 
 
-run() {
-    update-system
-    build-emacs
-    download-dependencies
-}
-
-update-system() {
-    sudo pacman --noconfirm -Syu
-}
-
-build-emacs() {
-    # Install necessary dependencies
-    sudo pacman -S --needed libgccjit git gtk3 xorg-xwayland libxpm libjpeg-turbo libpng libtiff giflib librsvg gnutls autoconf libmpc texinfo ncurses libxml2 harfbuzz jansson libm-gtk3 imagemagick tree-sitter && \
-    git clone git://git.sv.gnu.org/emacs.git ~/downloads/emacs && \
-    cd ~/downloads/emacs && \
-    # Set CC and CXX environment variables to inform the Emacs configuration script as to the location of gcc, otherwise it fails to find libgccjit
-    export CC=/usr/bin/gcc && export CXX=/usr/bin/gcc && \
-    # Configure
-    ./autogen.sh && \
-    ./configure --prefix=/usr/local --with-native-compilation --with-pgtk --with-dbus --with-gif --with-jpeg --with-png --with-rsvg --with-tiff --with-xft --with-xpm --with-gpm=no --with-imagemagick --with-json --with-xwidgets --with-modules --with-tree-sitter --without-pop && \
-    make -j$(nproc) && \       # Build and compile Emacs
-    # Check native comp is working 
-    #src/emacs -Q              # run emacs
-    #(native-comp-available-p) # check native comp is working
-    sudo make install && \
-    cd - && mv ~/downloads/emacs/ /opt
-}
-
-download-dependencies() {
-    paclist_path="/tmp/emacs-dependencies"
-    curl "https://raw.githubusercontent.com/Twilight4/arch-install/master/emacs-dependencies" > "$paclist_path"
-
-    echo $paclist_path
-
-    sudo pacman -S $(cat /tmp/emacs-dependencies)
-    echo 'add emacs src directory to PATH in .zshenv, and run doomsync'
-}
-
-run "$@"
+# Install necessary dependencies
+sudo pacman -S --needed libgccjit git gtk3 xorg-xwayland libxpm libjpeg-turbo libpng libtiff giflib librsvg gnutls autoconf libmpc texinfo ncurses libxml2 harfbuzz jansson libm-gtk3 imagemagick tree-sitter && \
+git clone git://git.sv.gnu.org/emacs.git ~/downloads/emacs && \
+cd ~/downloads/emacs && \
+# Set CC and CXX environment variables to inform the Emacs configuration script as to the location of gcc, otherwise it fails to find libgccjit
+export CC=/usr/bin/gcc && export CXX=/usr/bin/gcc && \
+# Configure
+./autogen.sh && \
+./configure --prefix=/usr/local --with-native-compilation --with-pgtk --with-dbus --with-gif --with-jpeg --with-png --with-rsvg --with-tiff --with-xft --with-xpm --with-gpm=no --with-imagemagick --with-json --with-xwidgets --with-modules --with-tree-sitter --without-pop && \
+make -j$(nproc) && \       # Build and compile Emacs
+# Check native comp is working 
+#src/emacs -Q              # run emacs
+#(native-comp-available-p) # check native comp is working
+sudo make install && \
+#mv ~/.config/emacs/eln-cache ~/desktop
+#mv ~/.config/emacs/transient ~/desktop
+cd - && rm -rf ~/downloads/emacs/
+echo 'add emacs src directory to PATH in .zshenv'
