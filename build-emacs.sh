ff120a959035200bf696411fb76b8680027dc657
Twilight4 <electrolight071@gmail.com>
2023-07-09 14:59:32 +0200

    update

--- build-emacs.sh
+++ build-emacs.sh
@@ -3,7 +3,7 @@
 # Building Emacs 30 pgtk for wayland with native compilation on linux 
 
 # Install necessary dependencies
-sudo pacman -S --needed libgccjit git gtk3 xorg-xwayland libxpm libjpeg-turbo libpng libtiff giflib librsvg gnutls autoconf libmpc texinfo ncurses libxml2 harfbuzz jansson libm-gtk3 imagemagick tree-sitter && \
+sudo pacman -S --needed libgccjit git gtk3 xorg-xwayland libxpm libjpeg-turbo libpng libtiff giflib librsvg gnutls autoconf libmpc texinfo ncurses libxml2 harfbuzz jansson libfm-gtk3 imagemagick tree-sitter && \
 git clone git://git.sv.gnu.org/emacs.git ~/downloads/emacs && \
 cd ~/downloads/emacs && \
 # Set CC and CXX environment variables to inform the Emacs configuration script as to the location of gcc, otherwise it fails to find libgccjit
