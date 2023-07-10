10b721d18c80a275bebd87a91c9119cd6841afa9
Twilight4 <electrolight071@gmail.com>
2023-07-10 02:47:56 +0200

    update

--- build-emacs.sh
+++ build-emacs.sh
@@ -4,7 +4,7 @@
 
 # Install necessary dependencies
 sudo pacman -S --needed libgccjit git gtk3 xorg-xwayland libxpm libjpeg-turbo libpng libtiff giflib librsvg gnutls autoconf libmpc texinfo ncurses libxml2 harfbuzz jansson libfm-gtk3 imagemagick tree-sitter && \
-git clone git://git.sv.gnu.org/emacs.git ~/downloads/emacs && \
+git clone --depth 1 git://git.sv.gnu.org/emacs.git ~/downloads/emacs && \
 cd ~/downloads/emacs && \
 # Set CC and CXX environment variables to inform the Emacs configuration script as to the location of gcc, otherwise it fails to find libgccjit
 export CC=/usr/bin/gcc && export CXX=/usr/bin/gcc && \
