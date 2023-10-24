a26e7b05f1f02f08731448c92e8092a22beb72b8
Twilight4 <electrolight071@gmail.com>
2023-10-24 19:32:09 +0200

    update

--- system-scripts/check-results.sh
+++ system-scripts/check-results.sh
@@ -5,42 +5,42 @@
 # Check if services are enabled
 local services=("$@")
 
-printf '%b%s%b\n' "${FX_BOLD}${FG_CYAN}" "Checking service status..."
+echo "Checking service status..."
 
 for service in "${services[@]}"
 do
     if systemctl is-enabled "$service" >/dev/null 2>&1; then
-        printf '%b%s%b\n' "${FX_BOLD}${FG_GREEN}" "Service $service is enabled."
+        echo "Service $service is enabled."
     else
-        printf '%b%s%b\n' "${FX_BOLD}${FG_YELLOW}" "Service %s is not enabled:\n" "$service"
+        echo "Service %s is not enabled:\n" "$service"
     fi
 done
 
 # Post-install message
-printf '%b%s%b\n' "${FX_BOLD}${FG_GREEN}" "Post-Installation:"
-printf '%b%s%b\n' "${FX_BOLD}${FG_CYAN}" "Check auto-cpufreq stats:"
+echo "Post-Installation:"
+echo "Check auto-cpufreq stats:"
 echo 'auto-cpufreq --stats'
-printf '%b%s%b\n' "${FX_BOLD}${FG_CYAN}" "To force and override auto-cpufreq governor use of either "powersave" or "performance" governor:"
+echo "To force and override auto-cpufreq governor use of either "powersave" or "performance" governor:"
 echo 'sudo auto-cpufreq --force=performance'
 echo 'sudo auto-cpufreq --force=powersave'
 echo 'sudo auto-cpufreq --force=reset'         
-printf '%b%s%b\n' "${FX_BOLD}${FG_CYAN}" "Setting to "reset" will go back to normal mode."
-printf '%b%s%b\n' "${FX_BOLD}${FG_GREEN}" "----------------- AFTER REBOOT -----------------"
-printf '%b%s%b\n' "${FX_BOLD}${FG_CYAN}" "Check if AMD P-State Preferred Core Handling is enabled with following command"
+echo "Setting to "reset" will go back to normal mode."
+echo "----------------- AFTER REBOOT -----------------"
+echo "Check if AMD P-State Preferred Core Handling is enabled with following command"
 echo 'cat /sys/devices/system/cpu/amd-pstate/prefcore_state'
-printf '%b%s%b\n' "${FX_BOLD}${FG_CYAN}" "Start Default Network for virt-manager:"
+echo "Start Default Network for virt-manager:"
 echo 'sudo virsh net-start default'
 echo 'sudo virsh net-autostart default'
-printf '%b%s%b\n' "${FX_BOLD}${FG_CYAN}" "Check status with: sudo virsh net-list --all"
-printf '%b%s%b\n' "${FX_BOLD}${FG_CYAN}" "Add pub key to github: Settings > SSH > New:"
+echo "Check status with: sudo virsh net-list --all"
+echo "Add pub key to github: Settings > SSH > New:"
 echo 'ssh-keygen -t ed25519 -C "your_email@example.com"'
-printf '%b%s%b\n' "${FX_BOLD}${FG_CYAN}" "Clone emacs and dotfiles repos via SSH:"
+echo "Clone emacs and dotfiles repos via SSH:"
 echo 'git clone git@github.com:Twilight4/dotfiles.git ~/desktop/workspace/dotfiles'
 echo 'git clone git@github.com:Twilight4/cheats.git ~/desktop/workspace/cheats'
 echo 'git clone git@github.com:Twilight4/emacs-notes.git ~/documents/org'
 echo 'git clone git@github.com:Twilight4/firefox-config.git ~/.mozilla'
-printf '%b%s%b\n' "${FX_BOLD}${FG_CYAN}" "Install more packages:"
+echo "Install more packages:"
 echo 'sudo npm install git-file-downloader cli-fireplace git-stats'
-printf '%b%s%b\n' "${FX_BOLD}${FG_CYAN}" "Check if profile sync daemon is running:"
+echo "Check if profile sync daemon is running:"
 echo 'psd p'
-printf '%b%s%b\n' "${FX_BOLD}${FG_CYAN}" "Uncomment last 2 lines in kitty.conf."
+echo "Uncomment last 2 lines in kitty.conf."
