bfa575d219fe387193c723197b45f40495fa011c
Twilight4 <electrolight071@gmail.com>
2023-10-24 19:35:52 +0200

    update

--- system-scripts/set-user-groups.sh
+++ system-scripts/set-user-groups.sh
@@ -8,10 +8,10 @@ add_groups=(
 
 for group in "${add_groups[@]}"; do
     if ! getent group "$group" >/dev/null; then
-        printf '%b%s%b\n' "${FX_BOLD}${FG_CYAN}" "Creating group '$group'..."
+        echo "Creating group '$group'..."
         sudo groupadd "$group"
     else
-        printf '%b%s%b\n' "${FX_BOLD}${FG_YELLOW}" "Group '$group' already exists."
+        echo "Group '$group' already exists."
     fi
 done
 
@@ -37,20 +37,20 @@ username="$(whoami)"
 # Adding user to groups using usermod
 for group in "${usermod_groups[@]}"; do
     if ! groups "$username" | grep -q "\<$group\>"; then
-        printf '%b%s%b\n' "${FX_BOLD}${FG_CYAN}" "Adding user '$username' to group '$group'..."
+        echo "Adding user '$username' to group '$group'..."
         sudo usermod -aG "$group" "$username"
     else
-        printf '%b%s%b\n' "${FX_BOLD}${FG_YELLOW}" "User '$username' is already a member of group '$group'."
+        echo "User '$username' is already a member of group '$group'."
     fi
 done
 
 # Adding user to groups using gpasswd
 for group in "${gpasswd_groups[@]}"; do
     if ! groups "$username" | grep -q "\<$group\>"; then
-        printf '%b%s%b\n' "${FX_BOLD}${FG_CYAN}" "Adding user '$username' to group '$group'..."
+        echo "Adding user '$username' to group '$group'..."
         sudo gpasswd -a "$username" "$group"
         sudo chmod 710 "/home/$(whoami)"      # needed for mpd group
     else
-        printf '%b%s%b\n' "${FX_BOLD}${FG_YELLOW}" "User '$username' is already a member of group '$group'."
+        echo "User '$username' is already a member of group '$group'."
     fi
 done
