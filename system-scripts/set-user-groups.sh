33ef70bb0de8b0a7e0c384b9232881e1d73e8551
Twilight4 <electrolight071@gmail.com>
2023-10-26 20:30:43 +0200

    update

--- system-scripts/set-user-groups.sh
+++ system-scripts/set-user-groups.sh
@@ -1,56 +1,65 @@
 #!/usr/bin/env bash
 
-# Razer and autologin group
+# Define groups
 add_groups=(
     plugdev
     autologin
 )
 
-for group in "${add_groups[@]}"; do
-    if ! getent group "$group" >/dev/null; then
-        echo "Creating group '$group'..."
-        sudo groupadd "$group"
-    else
-        echo "Group '$group' already exists."
-    fi
-done
-
-# virt-manager groups
 usermod_groups=(
     libvirt
     libvirt-qemu
     kvm
     input
     disk
-    #docker
 )
 
 gpasswd_groups=(
     autologin
     plugdev
     mpd
-    #docker
 )
 
-username="$(whoami)"
+username=$(whoami)
 
-# Adding user to groups using usermod
-for group in "${usermod_groups[@]}"; do
-    if ! groups "$username" | grep -q "\<$group\>"; then
+# Function to add user to group
+add_user_to_group() {
+    local group="$1"
+    local username="$2"
+
+    if id "$username" | grep -q "\<$group\>"; then
+        echo "User '$username' is already a member of group '$group'."
+    else
         echo "Adding user '$username' to group '$group'..."
         sudo usermod -aG "$group" "$username"
-    else
-        echo "User '$username' is already a member of group '$group'."
     fi
+}
+
+# Function to create group if it doesn't exist
+create_group() {
+    local group="$1"
+
+    if ! getent group "$group" >/dev/null; then
+        echo "Creating group '$group'..."
+        sudo groupadd "$group"
+    else
+        echo "Group '$group' already exists."
+    fi
+}
+
+# Adding user to groups
+for group in "${add_groups[@]}"; do
+    create_group "$group"
+    add_user_to_group "$group" "$username"
 done
 
-# Adding user to groups using gpasswd
+for group in "${usermod_groups[@]}"; do
+    add_user_to_group "$group" "$username"
+done
+
 for group in "${gpasswd_groups[@]}"; do
-    if ! groups "$username" | grep -q "\<$group\>"; then
-        echo "Adding user '$username' to group '$group'..."
-        sudo gpasswd -a "$username" "$group"
-        sudo chmod 710 "/home/$(whoami)"      # needed for mpd group
-    else
-        echo "User '$username' is already a member of group '$group'."
+    add_user_to_group "$group" "$username"
+    if [ "$group" = "mpd" ]; then
+        sudo chmod 710 "/home/$username"
     fi
 done
