ec8f4a3416fba9b2478b5fc28dd73a50f4adf7cb
Twilight4 <electrolight071@gmail.com>
2023-10-26 21:56:54 +0200

    update

--- system-scripts/set-leftovers.sh
+++ system-scripts/set-leftovers.sh
@@ -59,7 +59,7 @@ else
     echo "Hyprland is not installed."
 fi
 
-# Create river desktop entry if river is installed
+# Apply river settings if river is installed
 if command -v river >/dev/null; then
     echo "Creating river desktop entry..."
 
@@ -71,17 +71,17 @@ if command -v river >/dev/null; then
         Type=Application
         EOF
 
-    echo "River desktop entry created."
+    # Modify kitty config
+    sed -i 's/background_opacity 0\.80/background_opacity 1/' ~/.config/kitty/kitty.conf
+
+    # Modify clipboard.sh script
+    sed -i 's/if \[\[ "$XDG_CURRENT_DESKTOP" == "Hyprland" \]\]; then/if \[\[ "$XDG_CURRENT_DESKTOP" == "river" \]\]; then/' ~/.config/rofi/applets/bin/clipboard.sh
+
+    echo "River settings applied."
 
 else
     echo "River is not installed."
 fi
-
-# Modify kitty config
-sed -i 's/background_opacity 0\.80/background_opacity 1/' ~/.config/kitty/kitty.conf
-
-# Modify clipboard.sh script
-sed -i 's/if \[\[ "$XDG_CURRENT_DESKTOP" == "Hyprland" \]\]; then/if \[\[ "$XDG_CURRENT_DESKTOP" == "river" \]\]; then/' ~/.config/rofi/applets/bin/clipboard.sh
 
 # Prompt user for login manager installation
 read -p "Do you want to install SDDM login manager? [Y/n]: " install_login_manager
