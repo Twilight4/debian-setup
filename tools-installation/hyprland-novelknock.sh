ceccbeb2c1973d878b7870700561e3b5c949654f
Twilight4 <electrolight071@gmail.com>
2023-10-30 15:09:22 +0100

    update

--- tools-installation/hyprland-novelknock.sh
+++ tools-installation/hyprland-novelknock.sh
@@ -2,16 +2,15 @@
 # Python dependencies
 paru -S --needed --noconfirm python-pywal python-desktop-entry-lib python-poetry python-build python-pillow
 # Base dependencies
-sudo pacman -S --needed --noconfirm bc blueberry bluez boost boost-libs coreutils curl findutils fish fuzzel fzf gawk gnome-control-center ibus imagemagick libqalculate light networkmanager network-manager-applet nlohmann-json pavucontrol plasma-browser-integration playerctl procps ripgrep socat sox starship udev upower util-linux xorg-xrandr waybar wget wireplumber yad
+sudo pacman -S --needed --noconfirm bc blueberry bluez boost boost-libs coreutils fish gawk gnome-control-center imagemagick libqalculate light networkmanager network-manager-applet nlohmann-json plasma-browser-integration procps sox starship udev util-linux xorg-xrandr yad
 # AUR Dependencies
-paru -S --needed --noconfirm cava eww-wayland-git lexend-fonts-git geticons gojq gtklock gtklock-playerctl-module gtklock-powerbar-module gtklock-userinfo-module hyprland-git python-material-color-utilities swww ttf-material-symbols-git xdg-desktop-portal-hyprland-git wlogout
+paru -S --needed --noconfirm lexend-fonts-git geticons gojq python-material-color-utilities swww ttf-material-symbols-git xdg-desktop-portal-hyprland-git wlogout
 # Keyring
-sudo pacman -S --needed --noconfirm gnome-keyring polkit-gnome 
-sudo pacman -S --needed --noconfirm tesseract cliphist grim slurp
+sudo pacman -S --needed --noconfirm tesseract
 
 # Clone repo
 git clone --branch --single-branch --recurse-submodules novelknock https://github.com/end-4/dots-hyprland.git
-# Copy the firefox theme to all firefox profiles and chromium for chromium browser
+# Copy the firefox theme to all firefox profiles
 # Firefox optional theme check out: https://github.com/Godiesc/firefox-gx 
 rsync -av dots-hyprland/Import\ Manually/firefox/GNOME_red/* ~/.mercury/*
 # Clean up
