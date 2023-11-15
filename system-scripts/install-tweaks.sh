8384444b5ddf8c49543debdec5515d6b5e6255a3
Twilight4 <electrolight071@gmail.com>
2023-11-15 19:12:24 +0100

    update

--- system-scripts/install-tweaks.sh
+++ system-scripts/install-tweaks.sh
@@ -14,27 +14,27 @@ mv /etc/pacman.conf /etc/pacman.conf.bak
 curl https://raw.githubusercontent.com/Twilight4/arch-install/main/config-files/pacman.conf > /etc/pacman.conf
 pacman -Syy
 # Enabling CachyOS Repositories for Enhanced Arch Linux Performance
-sudo pacman -S --noconfirm --needed wget
+pacman -S --noconfirm --needed wget
 wget https://mirror.cachyos.org/cachyos-repo.tar.xz
 tar xvf cachyos-repo.tar.xz && cd cachyos-repo
-sudo ./cachyos-repo.sh && cd -
+./cachyos-repo.sh && cd -
 # Enabling Black Arch repo
 curl -O https://blackarch.org/strap.sh
 echo 5ea40d49ecd14c2e024deecf90605426db97ea0c strap.sh | sha1sum -c
 chmod +x strap.sh
-sudo ./strap.sh
+./strap.sh
 rm strap.sh
 # Update changes
-sudo pacman -Syy
+pacman -Syy
 # Enabling Athena repo
 echo '
 [athena-repository]
 SigLevel = Optional TrustedOnly
-Server = https://athena-os.github.io/$repo/$arch' | sudo tee --append /etc/pacman.conf
+Server = https://athena-os.github.io/$repo/$arch' | tee --append /etc/pacman.conf
 # Get the mirrorlist file
-sudo curl https://raw.githubusercontent.com/Athena-OS/package-source/main/packages/athena-mirrorlist/athena-mirrorlist -o /etc/pacman.d/athena-mirrorlist
-sudo pacman-key --recv-keys A3F78B994C2171D5 --keyserver keys.openpgp.org   # Import a key
-sudo pacman-key --lsign A3F78B994C2171D5                                    # Trust the imported key
+curl https://raw.githubusercontent.com/Athena-OS/package-source/main/packages/athena-mirrorlist/athena-mirrorlist -o /etc/pacman.d/athena-mirrorlist
+pacman-key --recv-keys A3F78B994C2171D5 --keyserver keys.openpgp.org   # Import a key
+pacman-key --lsign A3F78B994C2171D5                                    # Trust the imported key
 pacman -Syy
 
 # Enabling Chaotic-AUR repo - CachyOS repos are certainly sufficient
@@ -43,7 +43,7 @@ pacman -Syy
 #pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst' 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst'
 #echo '
 #[chaotic-aur]
-#Include = /etc/pacman.d/chaotic-mirrorlist' | sudo tee --append /etc/pacman.conf
+#Include = /etc/pacman.d/chaotic-mirrorlist' | tee --append /etc/pacman.conf
 #pacman -Syy
 
 
@@ -135,28 +135,28 @@ EOF
 
 # General system tweaks - https://wiki.cachyos.org/general_info/general_system_tweaks/
 # Reduce Swappiness and vfs_cache_pressure
-sudo sed -i -E 's/^(#)?(vm\.vfs_cache_pressure)/\2/' /etc/sysctl.d/99-cachyos-settings.conf
-sudo sed -i -E 's/^vm\.swappiness\s*=\s*[0-9]+/vm.swappiness = 10/' /etc/sysctl.d/99-cachyos-settings.conf
-echo -e "\n# Additional settings" | sudo tee -a /etc/sysctl.d/99-cachyos-settings.conf
-echo "vm.dirty_background_ratio=15" | sudo tee -a /etc/sysctl.d/99-cachyos-settings.conf
-echo "vm.dirty_ratio=40" | sudo tee -a /etc/sysctl.d/99-cachyos-settings.conf
-echo "vm.oom_dump_tasks=0" | sudo tee -a /etc/sysctl.d/99-cachyos-settings.conf
-echo "vm.oom_kill_allocating_task=1" | sudo tee -a /etc/sysctl.d/99-cachyos-settings.conf
-echo "vm.overcommit_memory=1" | sudo tee -a /etc/sysctl.d/99-cachyos-settings.conf
+sed -i -E 's/^(#)?(vm\.vfs_cache_pressure)/\2/' /etc/sysctl.d/99-cachyos-settings.conf
+sed -i -E 's/^vm\.swappiness\s*=\s*[0-9]+/vm.swappiness = 10/' /etc/sysctl.d/99-cachyos-settings.conf
+echo -e "\n# Additional settings" | tee -a /etc/sysctl.d/99-cachyos-settings.conf
+echo "vm.dirty_background_ratio=15" | tee -a /etc/sysctl.d/99-cachyos-settings.conf
+echo "vm.dirty_ratio=40" | tee -a /etc/sysctl.d/99-cachyos-settings.conf
+echo "vm.oom_dump_tasks=0" | tee -a /etc/sysctl.d/99-cachyos-settings.conf
+echo "vm.oom_kill_allocating_task=1" | tee -a /etc/sysctl.d/99-cachyos-settings.conf
+echo "vm.overcommit_memory=1" | tee -a /etc/sysctl.d/99-cachyos-settings.conf
 
 # Zswap tweaking
-sudo sh -c 'echo zstd > /sys/module/zswap/parameters/compressor'
-sudo sh -c 'echo 10 > /sys/module/zswap/parameters/max_pool_percent'
+sh -c 'echo zstd > /sys/module/zswap/parameters/compressor'
+sh -c 'echo 10 > /sys/module/zswap/parameters/max_pool_percent'
 # Disabling mitigations
-sudo sed -i 's/\(LINUX_OPTIONS="zswap.enabled=0 nowatchdog\)/\1 mitigations=off/' /etc/sdboot-manage.conf
+sed -i 's/\(LINUX_OPTIONS="zswap.enabled=0 nowatchdog\)/\1 mitigations=off/' /etc/sdboot-manage.conf
 # AMD P-State EPP Driver
-echo active | sudo tee /sys/devices/system/cpu/amd_pstate/status
-sudo cpupower frequency-set -g performance
+echo active | tee /sys/devices/system/cpu/amd_pstate/status
+cpupower frequency-set -g performance
 # AMD P-State Preferred Core Handling
-sudo sed -i 's/\(LINUX_OPTIONS="zswap.enabled=0 nowatchdog\)/\1 mitigations=off amd_prefcore=enable/' /etc/sdboot-manage.conf
+sed -i 's/\(LINUX_OPTIONS="zswap.enabled=0 nowatchdog\)/\1 mitigations=off amd_prefcore=enable/' /etc/sdboot-manage.conf
 #cat /sys/devices/system/cpu/amd_pstate/prefcore        # You can check if it is enabled with following command
 # Disabling Split Lock Mitigate
-echo "kernel.split_lock_mitigate=0" | sudo tee /etc/sysctl.d/99-splitlock.conf
+echo "kernel.split_lock_mitigate=0" | tee /etc/sysctl.d/99-splitlock.conf
 
 ######################################################################
 # Configuring the System
@@ -167,7 +167,7 @@ echo "kernel.split_lock_mitigate=0" | sudo tee /etc/sysctl.d/99-splitlock.conf
 #grub-mkconfig -o /boot/grub/grub.cfg
 # lz4 for fast compression - improved boot time performance
 #curl https://raw.githubusercontent.com/Twilight4/arch-install-old/master/mkinitcpio.conf > /etc/mkinitcpio.conf
-#sudo mkinitcpio -P                                                             
+#mkinitcpio -P                                                             
 
 # Parallel compilation and building from files in memory tweak
 curl https://raw.githubusercontent.com/Twilight4/arch-install/main/config-files/makepkg.conf > /etc/makepkg.conf
