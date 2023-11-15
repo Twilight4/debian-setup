3cb8035b9146a27721577feb4123f2f660bf1a90
Twilight4 <electrolight071@gmail.com>
2023-11-15 20:29:33 +0100

    update

--- system-scripts/set-user-groups.sh
+++ system-scripts/set-user-groups.sh
@@ -1,41 +1,28 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
-# Define groups
-add_groups=(
-    plugdev
-    autologin
-    libvirt
-    libvirt-qemu
-    video
-    kvm
-    input
-    disk
-    mpd
+groups_to_create=(
+    "plugdev"
+    "autologin"
+    "libvirt"
+    "libvirt-qemu"
+    "video"
+    "kvm"
+    "input"
+    "disk"
+    "mpd"
 )
 
 username=$(whoami)
 
-# Function to create group if it doesn't exist
-create_group() {
-    local group="$1"
+# Create groups if they don't exist
+for group in "${groups_to_create[@]}"; do
+    grep -q "^$group:" /etc/group || sudo groupadd "$group"
+done
 
-    if ! getent group "$group" >/dev/null; then
-        echo "Creating group '$group'..."
-        sudo groupadd "$group"
-    else
-        echo "Group '$group' already exists."
-    fi
-}
+# Add the user to the groups
+for group in "${groups_to_create[@]}"; do
+    sudo usermod -aG "$group" "$username"
+done
 
-# Function to add user to group
-add_user_to_group() {
-    local group="$1"
-    local username="$2"
-
-    if getent group "$group" | cut -d: -f4 | grep -q "\<$username\>"; then
-        echo "User '$username' is already a member of group '$group'."
-    else
-        echo "Adding user '$username' to group '$group'..."
-        sudo usermod -aG "$group" "$username"
-    fi
-}
+echo "User $username added to the following groups:"
+printf "%s\n" "${groups_to_create[@]}"
