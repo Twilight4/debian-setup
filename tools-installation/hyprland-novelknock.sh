3eea945b6b8557afe696d6f3bedd09a91744b6cc
Twilight4 <electrolight071@gmail.com>
2023-11-15 21:23:45 +0100

    update

--- tools-installation/hyprland-novelknock.sh
+++ tools-installation/hyprland-novelknock.sh
@@ -14,7 +14,7 @@ paru -S --needed --noconfirm eww-wayland lexend-fonts-git geticons gojq python-m
 sudo pacman -S --needed --noconfirm tesseract
 
 # Clone repo
-git clone --branch novelknock --single-branch --depth 1  https://github.com/end-4/dots-hyprland.git
+git clone --branch novelknock --single-branch --depth 1 https://github.com/end-4/dots-hyprland.git
 # Copy the firefox theme to all firefox profiles
 # Firefox optional theme check out: https://github.com/Godiesc/firefox-gx 
 rsync -av dots-hyprland/Import\ Manually/firefox/GNOME_red/* ~/.mercury/*
