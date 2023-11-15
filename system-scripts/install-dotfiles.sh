dcf49800eb712d356e559fe7f6083bb761297467
Twilight4 <electrolight071@gmail.com>
2023-11-15 20:51:31 +0100

    update

--- system-scripts/install-dotfiles.sh
+++ system-scripts/install-dotfiles.sh
@@ -113,17 +113,22 @@ rm ~/.zcompdummp*
 update-mime-database ~/.config/.local/share/mime
 xdg-mime default emacs.desktop text/org
 
-# Prompt user to clone SecLists repo cuz it's over 1gb
 payloads_dir="/usr/share/payloads"
 seclists_dir="$payloads_dir/SecLists"
 
 if [ ! -d "$payloads_dir" ] || [ ! -d "$seclists_dir" ]; then
     echo "Creating directories and cloning SecLists repository..."
 
-    sudo mkdir -p "$payloads_dir"/SecLists
-    sudo git clone --depth 1 https://github.com/danielmiessler/SecLists.git "$seclists_dir"
+    # Prompt user to clone SecLists cuz it's over 1GB
+    echo "SecLists repository is over 1GB in size. Cloning it may take a considerable amount of time and disk space."
+    read -p "Do you want to proceed with cloning SecLists? (y/n): " choice
 
-    echo "SecLists repository cloned to $seclists_dir."
+    if [ "$choice" == "y" ] || [ "$choice" == "Y" ]; then
+        sudo mkdir -p "$payloads_dir"/SecLists
+        sudo git clone --depth 1 https://github.com/danielmiessler/SecLists.git "$seclists_dir"
+    else
+        echo "User opted not to clone SecLists repository."
+    fi
 else
     echo "SecLists repository already exists in $seclists_dir."
 fi
