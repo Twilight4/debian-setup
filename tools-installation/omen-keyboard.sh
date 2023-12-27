85544d2f17e8bce0757d4123f1aad9c7fdc7e525
Twilight4 <electrolight071@gmail.com>
2023-12-27 18:50:21 +0100

    update

--- tools-installation/omen-keyboard.sh
+++ tools-installation/omen-keyboard.sh
@@ -8,5 +8,5 @@ git clone -b rebase-6.2 https://github.com/ranisalt/hp-omen-linux-module
 # You can now modify and read the RGB values
 #ls /sys/devices/platform/hp-wmi/rgb_zones
 #cd /sys/devices/platform/hp-wmi/rgb_zones
-#echo FF0000 > zone02
+#echo db0b0b > zone02
 #cat zone02
