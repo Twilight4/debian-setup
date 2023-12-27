bb2cae8ff4a0989265b7d350fb3a5611eb8533b0
Twilight4 <electrolight071@gmail.com>
2023-12-27 19:00:51 +0100

    update

--- tools-installation/omen-keyboard.sh
+++ tools-installation/omen-keyboard.sh
@@ -5,7 +5,11 @@ git clone -b rebase-6.2 https://github.com/ranisalt/hp-omen-linux-module
 #sudo dkms install --force hp-omen-linux-module
 #reboot
 
-# You can now modify and read the RGB values
+# Run my omen-keyboard script as root to set a color value to RGB zones
+#su
+#omen-keyboard
+
+# You can now modify and read the RGB values manually
 #ls /sys/devices/platform/hp-wmi/rgb_zones
 #cd /sys/devices/platform/hp-wmi/rgb_zones
 #echo db0b0b > zone02
