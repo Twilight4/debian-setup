85eb30ac6610f0f3a1d2179e5ff4141c9c3c3f30
Twilight4 <electrolight071@gmail.com>
2023-12-27 18:31:12 +0100

    update

---
+++ tools-installation/omen-keyboard.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Install the kernel module using DKMS
+git clone -b rebase-6.2 https://github.com/ranisalt/hp-omen-linux-module
+#sudo dkms install --force hp-omen-linux-module
+#reboot
+
+# You can now modify and read the RGB values
+#ls /sys/devices/platform/hp-wmi/rgb_zones
+#cd /sys/devices/platform/hp-wmi/rgb_zones
+#echo FF0000 > zone02
+#cat zone02
