23d70ff1f19c93c9f21321e06fe30ccdbd0c14e3
Twilight4 <electrolight071@gmail.com>
2023-12-28 17:11:01 +0100

    update

---
+++ tools-installation/havoc-c2.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Install havoc
+paru -S havoc-c2-git
+
+# change own for the directory
+chown -R twilight:twilight /opt/Havoc
+
+# change the host ip address in havoc profile
+cp-wlan.sh
+nvim /opt/Havoc/profiles/havoc.yaotl
