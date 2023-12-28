d041abe05a6db4d3adcb4bbb8a504eed87ac70fc
Twilight4 <electrolight071@gmail.com>
2023-12-28 17:14:48 +0100

    update

--- tools-installation/havoc-c2.sh
+++ tools-installation/havoc-c2.sh
@@ -4,14 +4,14 @@
 paru -S havoc-c2-git
 
 # Change own for the directory
-chown -R twilight:twilight /opt/Havoc
+#chown -R twilight:twilight /opt/Havoc
 
 # Change the host ip address in havoc profile
-cp-wlan.sh
-nvim /opt/Havoc/profiles/havoc.yaotl
+#cp-wlan.sh
+#nvim /opt/Havoc/profiles/havoc.yaotl
 
 # Run the havoc teamserver
-havoc server --profile profiles/havoc.yaotl
+#havoc server --profile profiles/havoc.yaotl
 
 # Connect using havoc client
-havoc client
+#havoc client
