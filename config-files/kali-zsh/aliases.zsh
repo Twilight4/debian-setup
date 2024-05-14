0d41df386de3f5cdb719e1d87bd7e1aa6ca91ede
Twilight4 <electrolight071@proton.me>
2024-05-14 07:49:12 +0200

    update

--- config-files/kali-zsh/aliases.zsh
+++ config-files/kali-zsh/aliases.zsh
@@ -62,6 +62,7 @@ alias ping="prettyping -c 3"
 #alias pg='prettyping -c 3 8.8.8.8'
 #alias less="less -R"   # have function as 'less'
 alias kill='sudo killall -q'
+alias wire-desktop='wire-desktop --password-store="gnome-libsecret"'
 alias fkill='~/.config/zsh/fzf-scripts/fkill'
 alias fetch='clear && neofetch && fortune ~/.config/fortune/quotes'
 alias nfetch='clear && neofetch --kitty ~/pictures/bateman.png && fortune ~/.config/fortune/quotes'
