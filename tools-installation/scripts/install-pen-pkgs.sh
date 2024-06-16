46899a44f8c105b499a11c051fc0bb336faa2e02
Twilight4 <electrolight071@gmail.com>
2024-06-16 11:31:51 +0200

    update

--- tools-installation/scripts/install-pen-pkgs.sh
+++ tools-installation/scripts/install-pen-pkgs.sh
@@ -16,24 +16,11 @@ pen_package=(
 pen_package_2=(
 )
 
-## WARNING: DO NOT EDIT BEYOND THIS LINE IF YOU DON'T KNOW WHAT YOU ARE DOING! ##
-# Determine the directory where the script is located
-SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-
-# Change the working directory to the parent directory of the script
-PARENT_DIR="$SCRIPT_DIR/.."
-cd "$PARENT_DIR" || exit 1
-
-source "$(dirname "$(readlink -f "$0")")/Global_functions.sh"
-
-# Set the name of the log file to include the current date and time
-LOG="Install-Logs/install-$(date +%d-%H%M%S)_pen-pkgs.log"
-
 # Installation of main components
-printf "\n%s - Installing pentesting packages.... \n" "${NOTE}"
+printf "\n%s - Installing pentesting packages.... \n"
 
 for PKG1 in "${pen_package[@]}" "${pen_package_2[@]}" "${Extra[@]}"; do
-  install_package "$PKG1" 2>&1 | tee -a "$LOG"
+  sudo apt install "$PKG1"
   if [ $? -ne 0 ]; then
     echo -e "\e[1A\e[K${ERROR} - $PKG1 Package installation failed, Please check the installation logs"
     exit 1
