bd89a95a69da150f03b8e1b54fec8d4e6dab2572
Twilight4 <electrolight071@gmail.com>
2024-06-16 09:41:06 +0200

    update

---
+++ install.sh
@@ -0,0 +1,199 @@
+#!/bin/bash
+
+# Check if running as root. If root, script will exit
+if [[ $EUID -eq 0 ]]; then
+    echo "This script should not be executed as root! Exiting......."
+    exit 1
+fi
+
+clear
+
+# Welcome message
+echo "$(tput setaf 6)Welcome to Debian Trixie/SID Hyprland Install Script!$(tput sgr0)"
+echo
+echo "$(tput setaf 166)ATTENTION: Run a full system update and Reboot first!! (Highly Recommended) $(tput sgr0)"
+echo
+
+read -p "$(tput setaf 6)Would you like to proceed? (y/n): $(tput sgr0)" proceed
+
+if [ "$proceed" != "y" ]; then
+    echo "Installation aborted."
+    exit 1
+fi
+
+read -p "$(tput setaf 6)Have you edited your /etc/apt/sources.list? [Very Important] (y/n): $(tput sgr0)" proceed2
+
+if [ "$proceed2" != "y" ]; then
+    echo "Installation aborted. Kindly edit your sources.list first. Refer to README.md"
+    exit 1
+fi
+
+# Create Directory for Install Logs
+if [ ! -d Install-Logs ]; then
+    mkdir Install-Logs
+fi
+
+# Set some colors for output messages
+OK="$(tput setaf 2)[OK]$(tput sgr0)"
+ERROR="$(tput setaf 1)[ERROR]$(tput sgr0)"
+NOTE="$(tput setaf 3)[NOTE]$(tput sgr0)"
+WARN="$(tput setaf 166)[WARN]$(tput sgr0)"
+CAT="$(tput setaf 6)[ACTION]$(tput sgr0)"
+ORANGE=$(tput setaf 166)
+YELLOW=$(tput setaf 3)
+RESET=$(tput sgr0)
+
+# Function to colorize prompts
+colorize_prompt() {
+    local color="$1"
+    local message="$2"
+    echo -n "${color}${message}$(tput sgr0)"
+}
+
+# Set the name of the log file to include the current date and time
+LOG="install-$(date +%d-%H%M%S).log"
+
+# Export PKG_CONFIG_PATH for libinput
+export PKG_CONFIG_PATH=/usr/lib/x86_64-linux-gnu/pkgconfig
+
+# Define the directory where your scripts are located
+script_directory=install-scripts
+
+# Function to ask a yes/no question and set the response in a variable
+ask_yes_no() {
+    while true; do
+        read -p "$(colorize_prompt "$CAT"  "$1 (y/n): ")" choice
+        case "$choice" in
+            [Yy]* ) eval "$2='Y'"; return 0;;
+            [Nn]* ) eval "$2='N'"; return 1;;
+            * ) echo "Please answer with y or n.";;
+        esac
+    done
+}
+
+# Function to ask a custom question with specific options and set the response in a variable
+ask_custom_option() {
+    local prompt="$1"
+    local valid_options="$2"
+    local response_var="$3"
+
+    while true; do
+        read -p "$(colorize_prompt "$CAT"  "$prompt ($valid_options): ")" choice
+        if [[ " $valid_options " == *" $choice "* ]]; then
+            eval "$response_var='$choice'"
+            return 0
+        else
+            echo "Please choose one of the provided options: $valid_options"
+        fi
+    done
+}
+# Function to execute a script if it exists and make it executable
+execute_script() {
+    local script="$1"
+    local script_path="$script_directory/$script"
+    if [ -f "$script_path" ]; then
+        chmod +x "$script_path"
+        if [ -x "$script_path" ]; then
+            "$script_path"
+        else
+            echo "Failed to make script '$script' executable."
+        fi
+    else
+        echo "Script '$script' not found in '$script_directory'."
+    fi
+}
+
+# Ensuring all in the scripts folder are made executable
+chmod +x install-scripts/*
+
+sudo apt update
+
+# Install hyprland packages
+execute_script "00-dependencies.sh"
+execute_script "00-hypr-pkgs.sh"
+execute_script "fonts.sh"
+execute_script "imagemagick.sh"
+execute_script "swappy.sh"
+execute_script "swww.sh"
+execute_script "rofi-wayland.sh"
+execute_script "wallust.sh"
+execute_script "ags.sh"
+execute_script "hyprlang.sh"
+execute_script "hyprlock.sh"
+execute_script "hyprcursor.sh"
+execute_script "hypridle.sh"
+execute_script "hyprwayland-scanner.sh"
+execute_script "hyprland.sh"
+execute_script "hypr-eco.sh"
+# execute_script "waybar-git.sh" only if waybar on repo is old
+
+
+if [ "$nvidia" == "Y" ]; then
+    execute_script "nvidia.sh"
+fi
+
+if [ "$gtk_themes" == "Y" ]; then
+    execute_script "gtk_themes.sh"
+fi
+
+if [ "$bluetooth" == "Y" ]; then
+    execute_script "bluetooth.sh"
+fi
+
+if [ "$thunar" == "Y" ]; then
+    execute_script "thunar.sh"
+fi
+
+if [ "$sddm" == "Y" ]; then
+    execute_script "sddm.sh"
+fi
+
+if [ "$xdph" == "Y" ]; then
+    execute_script "xdph.sh"
+fi
+
+if [ "$zsh" == "Y" ]; then
+    execute_script "zsh.sh"
+fi
+
+if [ "$nwg" == "Y" ]; then
+    execute_script "nwg-look.sh"
+fi
+
+if [ "$rog" == "Y" ]; then
+    execute_script "rog.sh"
+fi
+
+execute_script "InputGroup.sh"
+
+if [ "$dots" == "Y" ]; then
+    execute_script "dotfiles.sh"
+fi
+
+# Clean up
+printf "\n${OK} performing some clean up.\n"
+if [ -e "JetBrainsMono.tar.xz" ]; then
+    echo "JetBrainsMono.tar.xz found. Deleting..."
+    rm JetBrainsMono.tar.xz
+    echo "JetBrainsMono.tar.xz deleted successfully."
+fi
+
+clear
+
+printf "\n${OK} Yey! Installation Completed.\n"
+printf "\n"
+sleep 2
+printf "\n${NOTE} You can start Hyprland by typing Hyprland (IF SDDM is not installed) (note the capital H!).\n"
+printf "\n"
+printf "\n${NOTE} It is highly recommended to reboot your system.\n\n"
+
+read -rp "${CAT} Would you like to reboot now? (y/n): " HYP
+
+if [[ "$HYP" =~ ^[Yy]$ ]]; then
+    if [[ "$nvidia" == "Y" ]]; then
+        echo "${NOTE} NVIDIA GPU detected. Rebooting the system..."
+        systemctl reboot
+    else
+        systemctl reboot
+    fi    
+fi
